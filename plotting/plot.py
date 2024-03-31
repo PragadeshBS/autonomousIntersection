3100c65f300301a510e243ebb9ae47ba61f8ca68
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-31 09:38:49 +0530

    restructure files

--- plotting/plot.py
+++ plotting/plot.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-df=pd.read_csv('reward.csv', names=['speed reduction factor','reward'])
+df=pd.read_csv('plotting/data/reward.csv', names=['speed reduction factor','reward'])
 
 plt.plot(df['speed reduction factor'], df['reward'],color='blue', marker='o', markersize=5, markerfacecolor='red')
 
-plt.xlim(-0.1, 1.5)
-plt.ylim(-105, 10)
+# plt.xlim(-0.1, 1.5)
+# plt.ylim(-105, 10)
 
 plt.axhline(0, color='black', linestyle='-', linewidth=0.5) 
 plt.axvline(0, color='black', linestyle='-', linewidth=0.5) 
@@ -16,5 +15,5 @@ plt.title('SRF vs Reward')
 plt.xlabel('Speed Reduction Factor')
 plt.ylabel('Reward')
 
-plt.savefig("image2.png", format="png", dpi=600)
+plt.savefig("plotting/output/spf vs reward.png", format="png", dpi=600)
 plt.show()
